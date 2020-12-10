ac89a493a727c093add4da4345fc8a0dfa58d06b
isham <mail.to.isham@gmail.com>
2020-12-10 19:26:36 +0530

    init

--- app/src/main/java/com/example/androidDeviceDetails/services/CollectorService.kt
+++ app/src/main/java/com/example/androidDeviceDetails/services/CollectorService.kt
@@ -1,5 +1,6 @@
 package com.example.androidDeviceDetails.services
 
+import android.annotation.SuppressLint
 import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -8,17 +9,23 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.location.Location
+import android.location.LocationManager
 import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.IBinder
+import android.provider.Settings
 import android.telephony.PhoneStateListener
 import android.telephony.TelephonyManager
+import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.example.androidDeviceDetails.managers.AppUsage
 import com.example.androidDeviceDetails.managers.SignalChangeListener
 import com.example.androidDeviceDetails.receivers.AppStateReceiver
 import com.example.androidDeviceDetails.receivers.BatteryReceiver
 import com.example.androidDeviceDetails.receivers.WifiReceiver
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import java.util.*
 
 const val CHANNEL_ID = "androidDeviceDetails"
@@ -31,7 +38,11 @@ class CollectorService : Service() {
     private lateinit var mTelephonyManager: TelephonyManager
     private lateinit var mPhoneStateListener: SignalChangeListener
     private lateinit var mWifiReceiver: WifiReceiver
-
+    private lateinit var locationManager: LocationManager
+    private var hasGps = false
+    private var hasNetwork = false
+    private var locationGps: Location? = null
+    private var locationNetwork: Location? = null
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
@@ -42,6 +53,7 @@ class CollectorService : Service() {
         mPhoneStateListener = SignalChangeListener(this)
         mWifiReceiver = WifiReceiver(this)
         mTelephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
 
         mAppStateReceiver = AppStateReceiver()
         val filter = IntentFilter()
@@ -79,14 +91,13 @@ class CollectorService : Service() {
             },
             0, 1000 * 60 * timeInterval
         )
-
         mTelephonyManager.listen(mPhoneStateListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS)
         val intentWifi = IntentFilter()
         intentWifi.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)
 
         this.registerReceiver(mBatteryReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
         this.registerReceiver(mWifiReceiver, intentWifi)
-
+        getLocation()
         return super.onStartCommand(intent, flags, startId)
     }
 
@@ -97,6 +108,59 @@ class CollectorService : Service() {
         this.unregisterReceiver(mAppStateReceiver)
         timer.cancel()
         stopSelf()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation() {
+        hasGps = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        hasNetwork = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        Log.d("Location", "getLocation: $hasGps $hasNetwork ")
+        if (hasGps || hasNetwork) {
+            if (hasGps) {
+                Log.d("CodeAndroidLocation", "hasGps")
+                locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    5000,
+                    0F
+                ) { location ->
+                    Log.d("CodeAndroidLocation", "gpsLocation not null $location")
+                    locationGps = location
+//                    insert(locationGps, "GPS")
+                }
+            }
+            if (hasNetwork) {
+                Log.d("CodeAndroidLocation", "hasNetworkGps")
+                locationManager.requestLocationUpdates(
+                    LocationManager.NETWORK_PROVIDER,
+                    5000,
+                    0F
+                ) { location ->
+                    Log.d("CodeAndroidLocation", "networkLocation not null $location")
+                    locationNetwork = location
+//                    insert(locationNetwork, "Network")
+                }
+            }
+            if (locationGps != null && locationNetwork != null) {
+                Log.d("CodeAndroidLocation", "has both")
+                if (locationGps!!.accuracy > locationNetwork!!.accuracy) {
+//                    insert(locationNetwork, "mNetwork")
+                } else {
+//                    insert(locationGps, "mGPS")
+                }
+            }
+//            GlobalScope.launch {
+//                val result = getData()
+//                if (result.isNotEmpty()) {
+//                    for (i in result.indices) {
+//                        Log.d("Latitude", result[i].latitude.toString())
+//                        Log.d("Longitude", result[i].longitude.toString())
+//                        Log.d("Time", result[i].time.toString())
+//                    }
+//                }
+//            }
+        } else {
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        }
     }
 }
 
