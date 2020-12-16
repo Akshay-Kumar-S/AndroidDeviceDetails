f971c71544927daf0d6ce213b96dbb66da4d1f54
isham <mail.to.isham@gmail.com>
2020-12-16 17:01:23 +0530

    graph added

--- app/src/main/java/com/example/androidDeviceDetails/services/CollectorService.kt
+++ app/src/main/java/com/example/androidDeviceDetails/services/CollectorService.kt
@@ -19,11 +19,15 @@ import android.telephony.PhoneStateListener
 import android.telephony.TelephonyManager
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import com.example.androidDeviceDetails.LocationActivity
 import com.example.androidDeviceDetails.managers.AppUsage
 import com.example.androidDeviceDetails.managers.SignalChangeListener
+import com.example.androidDeviceDetails.models.LocationModel
+import com.example.androidDeviceDetails.models.RoomDB
 import com.example.androidDeviceDetails.receivers.AppStateReceiver
 import com.example.androidDeviceDetails.receivers.BatteryReceiver
 import com.example.androidDeviceDetails.receivers.WifiReceiver
+import com.fonfon.kgeohash.GeoHash
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import java.util.*
@@ -39,10 +43,12 @@ class CollectorService : Service() {
     private lateinit var mPhoneStateListener: SignalChangeListener
     private lateinit var mWifiReceiver: WifiReceiver
     private lateinit var locationManager: LocationManager
+    private lateinit var locationDatabase: RoomDB
     private var hasGps = false
     private var hasNetwork = false
     private var locationGps: Location? = null
     private var locationNetwork: Location? = null
+
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
@@ -54,6 +60,7 @@ class CollectorService : Service() {
         mWifiReceiver = WifiReceiver(this)
         mTelephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
         locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        locationDatabase = RoomDB.getDatabase()!!
 
         mAppStateReceiver = AppStateReceiver()
         val filter = IntentFilter()
@@ -125,7 +132,7 @@ class CollectorService : Service() {
                 ) { location ->
                     Log.d("CodeAndroidLocation", "gpsLocation not null $location")
                     locationGps = location
-//                    insert(locationGps, "GPS")
+                    insert(locationGps, "GPS")
                 }
             }
             if (hasNetwork) {
@@ -137,30 +144,40 @@ class CollectorService : Service() {
                 ) { location ->
                     Log.d("CodeAndroidLocation", "networkLocation not null $location")
                     locationNetwork = location
-//                    insert(locationNetwork, "Network")
+                    insert(locationNetwork, "Network")
                 }
             }
             if (locationGps != null && locationNetwork != null) {
                 Log.d("CodeAndroidLocation", "has both")
                 if (locationGps!!.accuracy > locationNetwork!!.accuracy) {
-//                    insert(locationNetwork, "mNetwork")
+                    insert(locationNetwork, "mNetwork")
                 } else {
-//                    insert(locationGps, "mGPS")
+                    insert(locationGps, "mGPS")
                 }
             }
-//            GlobalScope.launch {
-//                val result = getData()
-//                if (result.isNotEmpty()) {
-//                    for (i in result.indices) {
-//                        Log.d("Latitude", result[i].latitude.toString())
-//                        Log.d("Longitude", result[i].longitude.toString())
-//                        Log.d("Time", result[i].time.toString())
-//                    }
-//                }
-//            }
         } else {
             startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
         }
     }
+
+    fun insert(location: Location?, tag: String) {
+        val geoHash = GeoHash(
+            location!!.latitude,
+            location.longitude,
+            6
+        ).toString()
+        Log.d("Time", Calendar.getInstance().time.toString())
+        Log.d("CodeAndroidLocation", "Latitude : " + location.latitude)
+        Log.d("CodeAndroidLocation", "Longitude : " + location.longitude)
+        Log.d("CodeAndroidLocation", "GeoHash : $geoHash")
+        val locationObj = LocationModel(
+            0, location.latitude, location.longitude, geoHash,
+            Calendar.getInstance().time.toString()
+        )
+        GlobalScope.launch {
+            locationDatabase.locationDao().insertLocation(locationObj)
+        }
+    }
+
 }
 
