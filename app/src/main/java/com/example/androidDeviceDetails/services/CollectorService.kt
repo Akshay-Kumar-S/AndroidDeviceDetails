ff06d1f4b3e264f702986daebefa215385d4d13b
keapen <kurieneapenk@gmail.com>
2020-12-17 15:25:30 +0530

    added uninstall button, altered table structure to include systemApps, moved spinner to action bar

--- app/src/main/java/com/example/androidDeviceDetails/services/CollectorService.kt
+++ app/src/main/java/com/example/androidDeviceDetails/services/CollectorService.kt
@@ -42,6 +42,8 @@ class CollectorService : Service() {
         mPhoneStateListener = SignalChangeListener(this)
         mWifiReceiver = WifiReceiver()
         mTelephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+
+
         mAppStateReceiver = AppStateReceiver()
         val filter = IntentFilter()
         filter.addAction(Intent.ACTION_PACKAGE_ADDED)
