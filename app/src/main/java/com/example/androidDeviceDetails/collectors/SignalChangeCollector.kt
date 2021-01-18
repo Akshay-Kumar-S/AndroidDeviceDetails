f7b32b13c04c545cb5acc895b3360705c7c73fea
nithi <nithindominic1924@gmail.com>
2021-01-18 10:01:18 +0530

    cooker modified

--- app/src/main/java/com/example/androidDeviceDetails/collectors/SignalChangeCollector.kt
+++ app/src/main/java/com/example/androidDeviceDetails/collectors/SignalChangeCollector.kt
@@ -1,4 +1,4 @@
-package com.example.androidDeviceDetails.collectors
+package com.example.androidDeviceDetails.managers
 
 import android.content.Context
 import android.os.Build
@@ -7,9 +7,9 @@ import android.telephony.PhoneStateListener.LISTEN_NONE
 import android.telephony.PhoneStateListener.LISTEN_SIGNAL_STRENGTHS
 import com.example.androidDeviceDetails.DeviceDetailsApplication
 import com.example.androidDeviceDetails.base.BaseCollector
-import com.example.androidDeviceDetails.collectors.SignalChangeCollector.SignalChangeListener
-import com.example.androidDeviceDetails.database.RoomDB
-import com.example.androidDeviceDetails.models.signal.SignalRaw
+import com.example.androidDeviceDetails.managers.SignalChangeCollector.SignalChangeListener
+import com.example.androidDeviceDetails.models.RoomDB
+import com.example.androidDeviceDetails.models.signalModels.SignalRaw
 import com.example.androidDeviceDetails.utils.Signal
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -34,85 +34,105 @@ class SignalChangeCollector : BaseCollector() {
          * Listener which gets notified when a change in signal strength occurs.
          *  Method is called when the strength of signal changes.
          *  Listener collects current timestamp, signal, strength, cellInfo type and level.
-         *  These values are made into a [SignalRaw] and saved into the [RoomDB.signalDao].
+         *  These values are made into a [SignalEntity] and saved into the [RoomDB.signalDao].
          *  This listener requires [android.Manifest.permission.ACCESS_FINE_LOCATION] permission.
          **/
         override fun onSignalStrengthsChanged(signalStrength: SignalStrength) {
-            val signalRaw: SignalRaw
+            val signalEntity: SignalRaw
             var level = 0
             var strength = 0
             var type = ""
             val signalDB = RoomDB.getDatabase()
-
+            var isRoaming = false
+            var operatorName = ""
+            var countryCode = ""
+            var networkBand=""
+            val telephonyManager =
+                DeviceDetailsApplication.instance.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
                 when (val cellInfo = signalStrength.cellSignalStrengths[0]) {
                     is CellSignalStrengthLte -> {
                         strength = cellInfo.rsrp
                         level = cellInfo.level
                         type = "LTE"
+                        networkBand="4g"
                     }
                     is CellSignalStrengthGsm -> {
                         strength = cellInfo.dbm
                         level = cellInfo.level
                         type = "GSM"
+                        networkBand="2g"
                     }
                     is CellSignalStrengthCdma -> {
                         strength = cellInfo.cdmaDbm
                         type = "CDMA"
                         level = cellInfo.level
+                        networkBand="3g"
                     }
                     is CellSignalStrengthWcdma -> {
                         strength = cellInfo.dbm
                         type = "WCDMA"
                         level = cellInfo.level
+                        networkBand="3g"
                     }
                     is CellSignalStrengthNr -> {
                         strength = cellInfo.csiRsrp
                         type = "NR"
                         level = cellInfo.level
+                        networkBand="5g"
                     }
                     is CellSignalStrengthTdscdma -> {
                         strength = cellInfo.dbm
                         type = "TDSCDMA"
                         level = cellInfo.level
+                        networkBand="3g"
                     }
                 }
             } else {
                 try {
-                    val telephonyManager =
-                        DeviceDetailsApplication.instance.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
                     when (val cellInfo = telephonyManager.allCellInfo[0]) {
                         is CellInfoLte -> {
                             type = "LTE"
                             strength = cellInfo.cellSignalStrength.dbm
                             level = cellInfo.cellSignalStrength.level
+                            networkBand="4g"
                         }
                         is CellInfoGsm -> {
                             type = "GSM"
                             strength = cellInfo.cellSignalStrength.dbm
                             level = cellInfo.cellSignalStrength.level
+                            networkBand="2g"
                         }
                         is CellInfoCdma -> {
                             type = "CDMA"
                             strength = cellInfo.cellSignalStrength.dbm
                             level = cellInfo.cellSignalStrength.level
+                            networkBand="3g"
                         }
                         is CellInfoWcdma -> {
                             type = "WCDMA"
                             strength = cellInfo.cellSignalStrength.dbm
                             level = cellInfo.cellSignalStrength.level
+                            networkBand="3g"
                         }
                     }
                 } catch (e: SecurityException) {
                 }
             }
-            signalRaw = SignalRaw(
-                System.currentTimeMillis(), Signal.CELLULAR.ordinal, strength, type, level
+            isRoaming = telephonyManager.isNetworkRoaming
+            operatorName = telephonyManager.networkOperatorName
+            countryCode = telephonyManager.simCountryIso
+            signalEntity = SignalRaw(
+                System.currentTimeMillis(), Signal.CELLULAR.ordinal, strength, type, level,operatorName,isRoaming,networkBand,countryCode
             )
             GlobalScope.launch {
-                signalDB?.signalDao()?.insertAll(signalRaw)
+                signalDB?.signalDao()?.insertAll(signalEntity)
             }
         }
+    }
+
+    init {
+        start()
     }
 
     /**
@@ -120,6 +140,9 @@ class SignalChangeCollector : BaseCollector() {
      **/
     override fun start() {
         mTelephonyManager.listen(SignalChangeListener, LISTEN_SIGNAL_STRENGTHS)
+    }
+
+    override fun collect() {
     }
 
     /**
