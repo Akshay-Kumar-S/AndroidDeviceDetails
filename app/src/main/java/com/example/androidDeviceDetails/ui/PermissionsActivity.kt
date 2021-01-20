4b9ff93f1f830daddd54b63f849773f1c14648a9
Naveen <123naveen.mm@gmail.com>
2021-01-20 14:37:41 +0530

    small changes

--- app/src/main/java/com/example/androidDeviceDetails/ui/PermissionsActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/ui/PermissionsActivity.kt
@@ -1,7 +1,9 @@
 package com.example.androidDeviceDetails.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.AdapterView
 import androidx.databinding.DataBindingUtil
@@ -9,7 +11,6 @@ import com.example.androidDeviceDetails.R
 import com.example.androidDeviceDetails.adapters.PermissionsListAdapter
 import com.example.androidDeviceDetails.controller.ActivityController
 import com.example.androidDeviceDetails.databinding.ActivityPermissionsBinding
-import com.example.androidDeviceDetails.utils.Utils
 
 class PermissionsActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -38,11 +39,6 @@ class PermissionsActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    fun deleteApp(view: View) {
-        Utils.uninstallApp(view.tag as String, packageManager, this)
-    }
-
-
     override fun onClick(v: View?) {
         when (v!!.id) {
         }
@@ -51,10 +47,11 @@ class PermissionsActivity : AppCompatActivity(), View.OnClickListener {
     private fun showApps(parent: AdapterView<*>, position: Int) {
         val adapter = parent.adapter as PermissionsListAdapter
         val item = adapter.getItem(position)
-//        val infoIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val infoIntent = Intent(Settings.ACTION_PRIVACY_SETTINGS)
+        intent.putExtra("permission", item)
 //        infoIntent.addCategory(Intent.CATEGORY_DEFAULT)
 //        infoIntent.data = Uri.parse("package:${item?.get(position)}")
-//        startActivity(infoIntent)
+        startActivity(infoIntent)
     }
 
 }
