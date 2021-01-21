bae8c3ec5454ad0112b3aabe33e21defcd4fe55f
isham <mail.to.isham@gmail.com>
2021-01-21 15:25:05 +0530

    -added clustering feature

--- app/src/main/java/com/example/androidDeviceDetails/ui/LocationActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/ui/LocationActivity.kt
@@ -1,6 +1,9 @@
 package com.example.androidDeviceDetails.ui
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.ColorMatrix
+import android.graphics.ColorMatrixColorFilter
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -20,6 +23,7 @@ import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.views.CustomZoomButtonsDisplay.HorizontalPosition.RIGHT
 import org.osmdroid.views.CustomZoomButtonsDisplay.VerticalPosition.CENTER
 import org.osmdroid.views.MapView.getTileSystem
+import org.osmdroid.views.overlay.TilesOverlay
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -78,8 +82,58 @@ class LocationActivity : AppCompatActivity(), View.OnClickListener, OnItemClickL
                 getTileSystem().minLatitude,
                 0
             )
+            mapView.overlayManager.tilesOverlay.setColorFilter(test());
             mapView.zoomController.display.setPositions(false, RIGHT, CENTER)
         }
+
+    }
+
+
+    fun test(): ColorMatrixColorFilter {
+        val inverseMatrix = ColorMatrix(floatArrayOf(
+            -1.0f, 0.0f, 0.0f, 0.0f, 255f,
+            0.0f, -1.0f, 0.0f, 0.0f, 255f,
+            0.0f, 0.0f, -1.0f, 0.0f, 255f,
+            0.0f, 0.0f, 0.0f, 1.0f, 0.0f
+        ))
+        val destinationColor = Color.parseColor("#FF2A2A2A");
+        val lr = (255.0f - Color.red(destinationColor))/255.0f;
+        val lg = (255.0f - Color.green(destinationColor))/255.0f;
+        val lb = (255.0f - Color.blue(destinationColor))/255.0f;
+        val grayscaleMatrix = ColorMatrix(
+            floatArrayOf(
+                lr, lg, lb, 0F, 0F, //
+                lr, lg, lb, 0F, 0F, //
+                lr, lg, lb, 0F, 0F, //
+                0F, 0F, 0F, 0F, 255F, //
+            ))
+        grayscaleMatrix.preConcat(inverseMatrix);
+        val dr = Color.red(destinationColor);
+        val dg = Color.green(destinationColor);
+        val db = Color.blue(destinationColor);
+        val drf = dr / 255f
+        val dgf = dg / 255f
+        val dbf = db / 255f
+        val tintMatrix =  ColorMatrix(
+            floatArrayOf(
+                drf, 0F, 0F, 0F, 0F, //
+                0F, dgf, 0F, 0F, 0F, //
+                0F, 0F, dbf, 0F, 0F, //
+                0F, 0F, 0F, 1F, 0F, //
+            ))
+        tintMatrix.preConcat(grayscaleMatrix);
+        val lDestination = drf * lr + dgf * lg + dbf * lb;
+        val scale = 1f - lDestination;
+        val translate = 1 - scale * 0.5f;
+        val scaleMatrix =  ColorMatrix(
+            floatArrayOf(
+            scale, 0F, 0F, 0F, dr * translate, //
+                0F, scale, 0F, 0F, dg * translate, //
+                0F, 0F, scale, 0F, db * translate, //
+                0F, 0F, 0F, 1F, 0F, //
+            ))
+        scaleMatrix.preConcat(tintMatrix);
+       return ColorMatrixColorFilter(scaleMatrix);
     }
 
     private fun initDatePicker() {
