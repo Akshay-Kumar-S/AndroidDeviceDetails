6b1ed5ac80e387b0234d867f5dc51b18d0889f53
Naveen <123naveen.mm@gmail.com>
2021-01-22 12:32:34 +0530

    Base appinfo activity created

---
+++ app/src/main/java/com/example/androidDeviceDetails/ui/BaseAppInfoActivity.kt
@@ -0,0 +1,32 @@
+package com.example.androidDeviceDetails.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.androidDeviceDetails.R
+import com.example.androidDeviceDetails.databinding.ActivityBaseAppInfoBinding
+import com.example.androidDeviceDetails.databinding.ActivityMainBinding
+
+class BaseAppInfoActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityBaseAppInfoBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_base_app_info)
+        binding.apply{
+            binding.appInfo.button.setOnClickListener(this@BaseAppInfoActivity)
+            binding.appPermissions.button.setOnClickListener(this@BaseAppInfoActivity)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.appInfo -> startActivity(Intent(this, AppInfoActivity::class.java))
+            R.id.appPermissions -> startActivity(Intent(this, PermissionsActivity::class.java))
+        }
+    }
+
+
+}
