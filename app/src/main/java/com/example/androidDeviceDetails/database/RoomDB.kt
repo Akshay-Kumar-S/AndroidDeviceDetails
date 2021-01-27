e6ae1ee2e32253f0b3a4d0852f43beecc7e2169b
isham <mail.to.isham@gmail.com>
2021-01-27 13:32:28 +0530

    -refactoring

--- app/src/main/java/com/example/androidDeviceDetails/database/RoomDB.kt
+++ app/src/main/java/com/example/androidDeviceDetails/database/RoomDB.kt
@@ -5,8 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.example.androidDeviceDetails.DeviceDetailsApplication
-import com.example.androidDeviceDetails.models.location.ILocationDao
-import com.example.androidDeviceDetails.models.location.LocationModel
+import com.example.androidDeviceDetails.models.signal.SignalDao
+import com.example.androidDeviceDetails.models.signal.SignalRaw
+
 
 @Database(
     entities = [AppEventRaw::class, BatteryRaw::class, LocationModel::class, AppInfoRaw::class, AppHistoryRaw::class, SignalRaw::class, AppNetworkUsageRaw::class, DeviceNetworkUsageRaw::class],
--- app/src/main/java/com/example/androidDeviceDetails/interfaces/OnItemClickListener.kt
+++ app/src/main/java/com/example/androidDeviceDetails/interfaces/OnItemClickListener.kt
@@ -1,7 +1,7 @@
 package com.example.androidDeviceDetails.interfaces
 
-import com.example.androidDeviceDetails.models.location.LocationDisplayModel
+import com.example.androidDeviceDetails.models.location.LocationItemViewHolder
 
 interface OnItemClickListener {
-    fun onItemClicked(clickedItem: LocationDisplayModel)
+    fun onItemClicked(clickedItem: LocationItemViewHolder)
 }
