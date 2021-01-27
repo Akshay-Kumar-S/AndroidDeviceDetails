7dab387d42225cdef8079de5fd1aa17830a9ca17
keapen <kurieneapenk@gmail.com>
2021-01-27 17:13:23 +0530

    --completed review changes

--- app/src/main/java/com/example/androidDeviceDetails/adapters/AppTypeAdapter.kt
+++ app/src/main/java/com/example/androidDeviceDetails/adapters/AppTypeAdapter.kt
@@ -1,18 +1,12 @@
 package com.example.androidDeviceDetails.adapters
 
-
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 import com.example.androidDeviceDetails.fragments.AppTypeUser
 import com.example.androidDeviceDetails.viewModel.AppInfoAppTypeViewModel
 
-
-class AppTypeAdapter(
-    fm: FragmentManager,
-    private var totalTabs: Int,
-    private var appInfoAppTypeViewModel: AppInfoAppTypeViewModel
-) : FragmentPagerAdapter(fm) {
+class AppTypeAdapter(fm: FragmentManager, private var totalTabs: Int) : FragmentPagerAdapter(fm) {
 
     override fun getItem(position: Int): Fragment {
         return when (position) {
@@ -21,7 +15,6 @@ class AppTypeAdapter(
         }
     }
 
-    override fun getCount(): Int {
-        return totalTabs
-    }
+    override fun getCount() = totalTabs
+
 }
