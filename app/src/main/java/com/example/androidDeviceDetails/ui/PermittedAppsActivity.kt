6fbeea4e46c809084cf2e26f75927ad97a230b3c
Naveen <123naveen.mm@gmail.com>
2021-01-28 23:38:13 +0530

    Menu added for allowed and denied apps

--- app/src/main/java/com/example/androidDeviceDetails/ui/PermittedAppsActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/ui/PermittedAppsActivity.kt
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Settings
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.AdapterView
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import com.example.androidDeviceDetails.R
 import com.example.androidDeviceDetails.adapters.PermissionsListAdapter
 import com.example.androidDeviceDetails.controller.ActivityController
 import com.example.androidDeviceDetails.databinding.ActivityPermittedAppsBinding
+import com.example.androidDeviceDetails.models.appInfo.EventType
 import com.example.androidDeviceDetails.models.permissionsModel.PermittedAppList
 
 class PermittedAppsActivity : AppCompatActivity() {
@@ -37,6 +41,30 @@ class PermittedAppsActivity : AppCompatActivity() {
         binding.apply {
             permittedAppsListView.isEnabled = true
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.permitted_apps_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val title = findViewById<TextView>(R.id.filter_text)
+        var filter = 1
+        when (item.itemId) {
+            R.id.allowed -> {
+                title.text = "Allowed"
+                filter = 1
+            }
+            R.id.denied -> {
+                title.text = "Denied"
+                filter = 0
+            }
+            else -> super.onSupportNavigateUp()
+        }
+        controller.filterView(filter)
+
+        return true
     }
 
     private fun showApps(parent: AdapterView<*>, position: Int) {
