d8fa0700e0ba05d8e67368d69230ab77f7deb0a6
kit <christina.aswez@gmail.com>
2021-01-29 12:27:29 +0530

    added comments

--- app/src/main/java/com/example/androidDeviceDetails/models/permissionsModel/PermittedAppList.kt
+++ app/src/main/java/com/example/androidDeviceDetails/models/permissionsModel/PermittedAppList.kt
@@ -1,5 +1,14 @@
 package com.example.androidDeviceDetails.models.permissionsModel
 
+/**
+ * A data class used to populate Permissions App UI
+ *
+ * @param package_name Package name of the app
+ * @param apk_title Title of the app
+ * @param version_name Version name of the app
+ * @param allowed_permissions String of allowed permissions
+ * @param denied_permissions String of denied permissions
+ */
 data class PermittedAppList(
     var package_name:String,
     var apk_title:String,
