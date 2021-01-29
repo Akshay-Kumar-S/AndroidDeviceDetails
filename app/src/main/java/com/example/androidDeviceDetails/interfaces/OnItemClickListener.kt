88adf403b0e2f75d5afe9cfae2903ba34cfaacba
neena38 <neena.mec@gmail.com>
2021-01-29 08:19:46 +0530

    resolving conflicts

--- app/src/main/java/com/example/androidDeviceDetails/interfaces/OnItemClickListener.kt
+++ app/src/main/java/com/example/androidDeviceDetails/interfaces/OnItemClickListener.kt
@@ -1,7 +1,6 @@
 package com.example.androidDeviceDetails.interfaces
 
 import com.example.androidDeviceDetails.models.location.LocationDisplayModel
-import com.example.androidDeviceDetails.models.location.LocationItemViewHolder
 
 interface OnItemClickListener {
     fun onItemClicked(clickedItem: LocationDisplayModel)
--- app/src/main/java/com/example/androidDeviceDetails/models/CardItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.androidDeviceDetails.models
-
-data class CardItem (
-    var image: Int = 0,
-    var name: String? = null
-)
