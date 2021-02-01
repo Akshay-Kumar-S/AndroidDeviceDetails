eef7101633bb98a0fa57369ca6c2983d54fd6757
Naveen <123naveen.mm@gmail.com>
2021-02-02 02:13:33 +0530

    Tab Layout added for app permissions

--- app/src/main/java/com/example/androidDeviceDetails/ui/PermittedAppsActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/ui/PermittedAppsActivity.kt
@@ -1,26 +1,18 @@
 package com.example.androidDeviceDetails.ui
 
-import android.content.Intent
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.Settings
-import android.util.Log
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.AdapterView
-import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.androidDeviceDetails.R
-import com.example.androidDeviceDetails.adapters.PermittedAppsAdapter
 import com.example.androidDeviceDetails.controller.ActivityController
 import com.example.androidDeviceDetails.databinding.ActivityPermittedAppsBinding
-import com.example.androidDeviceDetails.models.permissionsModel.PermittedAppList
+import com.example.androidDeviceDetails.models.permissionsModel.PermittedAppsCookedData
 
 class PermittedAppsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPermittedAppsBinding
-    private lateinit var controller: ActivityController<PermittedAppList>
+    private lateinit var controller: ActivityController<PermittedAppsCookedData>
 
     companion object {
         const val NAME = "permittedApps"
@@ -32,52 +24,10 @@ class PermittedAppsActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_permitted_apps)
         this.title = intent.getStringExtra("permission").toString();
         PERMISSION = intent.getStringExtra("permission").toString()
-        controller = ActivityController(
-            NAME,
-            binding,
-            this,
-            null,
-            supportFragmentManager
+        controller = ActivityController(NAME, binding, this, null, supportFragmentManager)
+        binding.tabLayout.setTabTextColors(
+            ContextCompat.getColor(this, R.color.white),
+            ContextCompat.getColor(this, R.color.white)
         )
-        Log.d("Tag", intent.getStringExtra("permission").toString())
-        binding.apply {
-            permittedAppsListView.setOnItemClickListener { parent, _, position, _ ->
-                showApps(parent, position)
-            }
-            permittedAppsListView.isEnabled = true
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.permitted_apps_menu, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        val title = findViewById<TextView>(R.id.filter_text)
-        var filter = 1
-        when (item.itemId) {
-            R.id.allowed -> {
-                title.text = "Allowed"
-                filter = 1
-            }
-            R.id.denied -> {
-                title.text = "Denied"
-                filter = 0
-            }
-            else -> super.onSupportNavigateUp()
-        }
-        controller.filterView(filter)
-
-        return true
-    }
-
-    private fun showApps(parent: AdapterView<*>, position: Int) {
-        val adapter = parent.adapter as PermittedAppsAdapter
-        val item = adapter.getItem(position)
-        val infoIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-        infoIntent.addCategory(Intent.CATEGORY_DEFAULT)
-        infoIntent.data = Uri.parse("package:${item?.package_name}")
-        startActivity(infoIntent)
     }
 }
--- app/src/main/java/com/example/androidDeviceDetails/viewModel/PermittedAppsViewModel.kt
+++ app/src/main/java/com/example/androidDeviceDetails/viewModel/PermittedAppsViewModel.kt
@@ -1,66 +1,131 @@
 package com.example.androidDeviceDetails.viewModel
+//
+//import android.content.Context
+//import com.example.androidDeviceDetails.R
+//import com.example.androidDeviceDetails.adapters.PermittedAppsAdapter
+//import com.example.androidDeviceDetails.base.BaseViewModel
+//import com.example.androidDeviceDetails.databinding.ActivityPermittedAppsBinding
+//import com.example.androidDeviceDetails.DeviceDetailsApplication
+//import com.example.androidDeviceDetails.models.permissionsModel.PermittedAppList
+//import com.example.androidDeviceDetails.models.permissionsModel.PermittedAppsCookedData
+//import com.example.androidDeviceDetails.viewModel.AppInfoViewModel.Companion.savedAppList
+//
+///**
+// * Implements [BaseViewModel]
+// */
+//class PermittedAppsViewModel(
+//    private val binding: ActivityPermittedAppsBinding,
+//    val context: Context
+//) :
+//    BaseViewModel() {
+//    companion object {
+//        var eventFilter = 1
+//        var savedAppList = arrayListOf<PermittedAppsCookedData>()
+//    }
+//
+//    /**
+//     * Displays provided data on UI as List view and a donut chart
+//     *
+//     * Overrides : [onDone] in [BaseViewModel]
+//     * @param [outputList] list of cooked data
+//     */
+//    @Suppress("UNCHECKED_CAST")
+//    override fun <T> onDone(outputList: ArrayList<T>) {
+//        val appList = outputList as ArrayList<PermittedAppsCookedData>
+//        var filteredList = appList.toMutableList()
+//        savedAppList = appList
+//        filteredList = filteredList.sortedBy { it.apk_title }.toMutableList()
+//        filteredList.removeAll { it.package_name == DeviceDetailsApplication.instance.packageName }
+//        val savedAppList: MutableList<PermittedAppsCookedData> = ArrayList()
+//        if (eventFilter == 1) {
+//            savedAppList.clear()
+//            for (i in filteredList) {
+//                if (i.isAllowed)
+//                    savedAppList.add(i)
+//            }
+//        } else {
+//            savedAppList.clear()
+//            for (i in filteredList) {
+//                if (!i.isAllowed)
+//                    savedAppList.add(i)
+//            }
+//        }
+//        binding.root.post {
+//            binding.permittedAppsListView.adapter =
+//                PermittedAppsAdapter(context, R.layout.permitted_app_info_tile, savedAppList)
+//        }
+//    }
+//
+//    /**
+//     * Filters [savedAppList] based on given filter type
+//     *
+//     * Overrides : [onDone] in [BaseViewModel]
+//     * @param [type] Type of filter
+//     */
+//    override fun filter(type: Int) {
+//        eventFilter = type
+//        onDone(savedAppList)
+//    }
+//}
 
 import android.content.Context
-import com.example.androidDeviceDetails.R
-import com.example.androidDeviceDetails.adapters.PermittedAppsAdapter
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidDeviceDetails.adapters.PermittedAppsFragmentAdapter
 import com.example.androidDeviceDetails.base.BaseViewModel
 import com.example.androidDeviceDetails.databinding.ActivityPermittedAppsBinding
-import com.example.androidDeviceDetails.DeviceDetailsApplication
 import com.example.androidDeviceDetails.models.permissionsModel.PermittedAppsCookedData
-import com.example.androidDeviceDetails.viewModel.AppInfoViewModel.Companion.savedAppList
+import com.google.android.material.tabs.TabLayout
+
 
 /**
  * Implements [BaseViewModel]
  */
-class PermittedAppsViewModel(private val binding: ActivityPermittedAppsBinding, val context: Context) :
+class PermittedAppsViewModel(val binding: ActivityPermittedAppsBinding, val context: Context) :
     BaseViewModel() {
-    companion object {
-        var eventFilter = 1
-        var savedAppList = arrayListOf<PermittedAppsCookedData>()
-    }
 
     /**
-     * Displays provided data on UI as List view and a donut chart
+     * Displays provided data on UI as List view
      *
      * Overrides : [onDone] in [BaseViewModel]
      * @param [outputList] list of cooked data
      */
-    @Suppress("UNCHECKED_CAST")
     override fun <T> onDone(outputList: ArrayList<T>) {
-        val appList = outputList as ArrayList<PermittedAppsCookedData>
-        var filteredList = appList.toMutableList()
-        savedAppList = appList
-        filteredList = filteredList.sortedBy { it.apk_title }.toMutableList()
-        filteredList.removeAll { it.package_name == DeviceDetailsApplication.instance.packageName }
-        val savedAppList: MutableList<PermittedAppsCookedData> = ArrayList()
-        if(eventFilter==1){
-            savedAppList.clear()
-            for(i in filteredList){
-                if(i.isAllowed)
-                    savedAppList.add(i)
-            }
-        }
-        else{
-            savedAppList.clear()
-            for(i in filteredList){
-                if(!i.isAllowed)
-                    savedAppList.add(i)
-            }
-        }
+        var appList =
+            (outputList.filterIsInstance<PermittedAppsCookedData>() as ArrayList).toMutableList()
+        appList = appList.sortedBy { it.package_name }.toMutableList()
         binding.root.post {
-            binding.permittedAppsListView.adapter =
-                PermittedAppsAdapter(context, R.layout.permitted_app_info_tile, savedAppList)
+            binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Allowed"))
+            binding.tabLayout.addTab(binding.tabLayout.newTab().setText("Denied"))
+            binding.tabLayout.tabGravity = TabLayout.GRAVITY_FILL
+            val adapter = PermittedAppsFragmentAdapter(
+                (context as AppCompatActivity).supportFragmentManager, binding.tabLayout.tabCount,
+                divideList(appList)
+            )
+            binding.viewPager.adapter = adapter
+            binding.viewPager.addOnPageChangeListener(
+                TabLayout.TabLayoutOnPageChangeListener(
+                    binding.tabLayout
+                )
+            )
+            binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                override fun onTabSelected(tab: TabLayout.Tab) {
+                    binding.viewPager.currentItem = tab.position
+                }
+
+                override fun onTabUnselected(tab: TabLayout.Tab) {}
+                override fun onTabReselected(tab: TabLayout.Tab) {}
+            })
         }
     }
 
-    /**
-     * Filters [savedAppList] based on given filter type
-     *
-     * Overrides : [onDone] in [BaseViewModel]
-     * @param [type] Type of filter
-     */
-    override fun filter(type: Int) {
-        eventFilter = type
-        onDone(savedAppList)
+    private fun divideList(appList: MutableList<PermittedAppsCookedData>): Pair<List<PermittedAppsCookedData>, List<PermittedAppsCookedData>> {
+        val convertedList = appList.map {
+            PermittedAppsCookedData(
+                it.package_name, it.apk_title, it.version_name,
+                it.isAllowed
+            )
+        }
+        return convertedList.partition { !it.isAllowed }
     }
+
 }
