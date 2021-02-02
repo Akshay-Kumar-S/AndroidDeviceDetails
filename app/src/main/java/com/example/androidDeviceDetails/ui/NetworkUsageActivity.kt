e8737cbc2a8db34bb7df817a8c8e13c0583d2715
Richard Joy <richardjoy530@gmail.com>
2021-02-02 18:21:22 +0530

    - Integrated DateTimePicker.kt in NetworkUsageActivity.kt

--- app/src/main/java/com/example/androidDeviceDetails/ui/NetworkUsageActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/ui/NetworkUsageActivity.kt
@@ -3,7 +3,6 @@ package com.example.androidDeviceDetails.ui
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.androidDeviceDetails.R
@@ -11,10 +10,10 @@ import com.example.androidDeviceDetails.controller.ActivityController
 import com.example.androidDeviceDetails.database.AppNetworkUsageRaw
 import com.example.androidDeviceDetails.databinding.ActivityAppDataBinding
 import com.example.androidDeviceDetails.fragments.SortBySheet
+import com.example.androidDeviceDetails.utils.DateTimePicker
 import com.example.androidDeviceDetails.utils.SortBy
-import java.util.*
 
-class NetworkUsageActivity : AppCompatActivity(), View.OnClickListener {
+class NetworkUsageActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAppDataBinding
     private lateinit var controller: ActivityController<AppNetworkUsageRaw>
     private lateinit var sortBySheet: SortBySheet
@@ -28,21 +27,7 @@ class NetworkUsageActivity : AppCompatActivity(), View.OnClickListener {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_app_data)
         controller = ActivityController(this, NAME, binding)
         sortBySheet = SortBySheet(options, controller::sortView, SortBy.WIFI_DESCENDING.ordinal)
-        binding.apply {
-            pickerBinding.startTime.setOnClickListener(this@NetworkUsageActivity)
-            pickerBinding.startDate.setOnClickListener(this@NetworkUsageActivity)
-            pickerBinding.endTime.setOnClickListener(this@NetworkUsageActivity)
-            pickerBinding.endDate.setOnClickListener(this@NetworkUsageActivity)
-        }
-    }
-
-    override fun onClick(v: View?) {
-        when (v!!.id) {
-            R.id.startTime -> controller.setTime(this, R.id.startTime)
-            R.id.startDate -> controller.setDate(this, R.id.startDate)
-            R.id.endTime -> controller.setTime(this, R.id.endTime)
-            R.id.endDate -> controller.setDate(this, R.id.endDate)
-        }
+        DateTimePicker(this, binding.pickerBinding, controller::setTime, controller::setDate)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
