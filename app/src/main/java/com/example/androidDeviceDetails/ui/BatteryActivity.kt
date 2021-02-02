a5fde43b131d2b86383bb9fad94fa0d3b514718b
Richard Joy <richardjoy530@gmail.com>
2021-02-02 18:19:10 +0530

    - Integrated DateTimePicker.kt in BatteryActivity.kt

--- app/src/main/java/com/example/androidDeviceDetails/ui/BatteryActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/ui/BatteryActivity.kt
@@ -6,7 +6,6 @@ import android.os.Bundle
 import android.provider.Settings
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
 import android.widget.AdapterView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -16,10 +15,10 @@ import com.example.androidDeviceDetails.controller.ActivityController
 import com.example.androidDeviceDetails.databinding.ActivityBatteryBinding
 import com.example.androidDeviceDetails.fragments.SortBySheet
 import com.example.androidDeviceDetails.models.battery.BatteryAppEntry
+import com.example.androidDeviceDetails.utils.DateTimePicker
 import com.example.androidDeviceDetails.utils.SortBy
-import java.util.*
 
-class BatteryActivity : AppCompatActivity(), View.OnClickListener {
+class BatteryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityBatteryBinding
     private lateinit var controller: ActivityController<BatteryAppEntry>
@@ -36,23 +35,9 @@ class BatteryActivity : AppCompatActivity(), View.OnClickListener {
             getString(R.string.battery_PkgName_REV_APLH) to SortBy.REVERSE_ALPHABETICAL.ordinal
         )
         sortBySheet = SortBySheet(options, controller::sortView, SortBy.DESCENDING.ordinal)
-        binding.apply {
-            listView.setOnItemClickListener { parent, _, position, _ ->
-                redirectToAppInfo(parent, position)
-            }
-            pickerBinding.startTime.setOnClickListener(this@BatteryActivity)
-            pickerBinding.startDate.setOnClickListener(this@BatteryActivity)
-            pickerBinding.endTime.setOnClickListener(this@BatteryActivity)
-            pickerBinding.endDate.setOnClickListener(this@BatteryActivity)
-        }
-    }
-
-    override fun onClick(v: View?) {
-        when (v!!.id) {
-            R.id.startTime -> controller.setTime(this, R.id.startTime)
-            R.id.startDate -> controller.setDate(this, R.id.startDate)
-            R.id.endTime -> controller.setTime(this, R.id.endTime)
-            R.id.endDate -> controller.setDate(this, R.id.endDate)
+        DateTimePicker(this, binding.pickerBinding, controller::setTime, controller::setDate)
+        binding.listView.setOnItemClickListener { parent, _, position, _ ->
+            redirectToAppInfo(parent, position)
         }
     }
 
@@ -68,8 +53,7 @@ class BatteryActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun redirectToAppInfo(parent: AdapterView<*>, position: Int) {
-        val adapter = parent.adapter as BatteryListAdapter
-        val item = adapter.getItem(position)
+        val item = (parent.adapter as BatteryListAdapter).getItem(position)
         val infoIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
         infoIntent.addCategory(Intent.CATEGORY_DEFAULT)
         infoIntent.data = Uri.parse("package:${item?.packageId}")
