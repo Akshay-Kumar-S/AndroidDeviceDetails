40910be1a611b04c40e87d45bc9eac896043745d
isham <mail.to.isham@gmail.com>
2021-02-02 17:22:37 +0530

    moved color filter new utils.ColorFilter

--- app/src/main/java/com/example/androidDeviceDetails/ui/LocationActivity.kt
+++ app/src/main/java/com/example/androidDeviceDetails/ui/LocationActivity.kt
@@ -1,9 +1,6 @@
 package com.example.androidDeviceDetails.ui
 
 import android.content.res.Configuration.*
-import android.graphics.Color
-import android.graphics.ColorMatrix
-import android.graphics.ColorMatrixColorFilter
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +12,7 @@ import com.example.androidDeviceDetails.controller.ActivityController
 import com.example.androidDeviceDetails.databinding.ActivityLocationBinding
 import com.example.androidDeviceDetails.interfaces.OnItemClickListener
 import com.example.androidDeviceDetails.models.location.LocationData
+import com.example.androidDeviceDetails.utils.ColorFilter
 import com.example.androidDeviceDetails.utils.SortBy
 import com.example.androidDeviceDetails.viewModel.LocationViewModel
 import com.google.android.material.bottomsheet.BottomSheetBehavior
@@ -70,61 +68,9 @@ class LocationActivity : AppCompatActivity(), View.OnClickListener, OnItemClickL
                 getTileSystem().maxLatitude, getTileSystem().minLatitude, 0
             )
             when (resources?.configuration?.uiMode?.and(UI_MODE_NIGHT_MASK)) {
-                UI_MODE_NIGHT_YES -> overlayManager.tilesOverlay.setColorFilter(mapColorFilter())
+                UI_MODE_NIGHT_YES -> overlayManager.tilesOverlay.setColorFilter(ColorFilter.darkModeFilter)
             }
         }
-    }
-
-    private fun mapColorFilter(): ColorMatrixColorFilter {
-        val inverseMatrix = ColorMatrix(
-            floatArrayOf(
-                -1.0f, 0.0f, 0.0f, 0.0f, 255f,
-                0.0f, -1.0f, 0.0f, 0.0f, 255f,
-                0.0f, 0.0f, -1.0f, 0.0f, 255f,
-                0.0f, 0.0f, 0.0f, 1.0f, 0.0f
-            )
-        )
-        val destinationColor = Color.parseColor("#FF2A2A2A")
-        val lr = (255.0f - Color.red(destinationColor)) / 255.0f
-        val lg = (255.0f - Color.green(destinationColor)) / 255.0f
-        val lb = (255.0f - Color.blue(destinationColor)) / 255.0f
-        val grayscaleMatrix = ColorMatrix(
-            floatArrayOf(
-                lr, lg, lb, 0F, 0F,
-                lr, lg, lb, 0F, 0F,
-                lr, lg, lb, 0F, 0F,
-                0F, 0F, 0F, 0F, 255F,
-            )
-        )
-        grayscaleMatrix.preConcat(inverseMatrix)
-        val dr = Color.red(destinationColor)
-        val dg = Color.green(destinationColor)
-        val db = Color.blue(destinationColor)
-        val drf = dr / 255f
-        val dgf = dg / 255f
-        val dbf = db / 255f
-        val tintMatrix = ColorMatrix(
-            floatArrayOf(
-                drf, 0F, 0F, 0F, 0F,
-                0F, dgf, 0F, 0F, 0F,
-                0F, 0F, dbf, 0F, 0F,
-                0F, 0F, 0F, 1F, 0F,
-            )
-        )
-        tintMatrix.preConcat(grayscaleMatrix)
-        val lDestination = drf * lr + dgf * lg + dbf * lb
-        val scale = 1f - lDestination
-        val translate = 1 - scale * 0.5f
-        val scaleMatrix = ColorMatrix(
-            floatArrayOf(
-                scale, 0F, 0F, 0F, dr * translate,
-                0F, scale, 0F, 0F, dg * translate,
-                0F, 0F, scale, 0F, db * translate,
-                0F, 0F, 0F, 1F, 0F,
-            )
-        )
-        scaleMatrix.preConcat(tintMatrix)
-        return ColorMatrixColorFilter(scaleMatrix)
     }
 
     private fun initOnClickListeners() {
