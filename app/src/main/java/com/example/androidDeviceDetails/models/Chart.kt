4db780dfe7c8ca009c153c5478a1020c6bd644da
neena38 <neena.mec@gmail.com>
2021-02-03 19:50:44 +0530

    Modified signal change listener

--- app/src/main/java/com/example/androidDeviceDetails/models/Chart.kt
+++ app/src/main/java/com/example/androidDeviceDetails/models/Chart.kt
@@ -5,6 +5,7 @@ import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
 data class Chart(
     var chartView: AAChartView,
     var title: String,
+    var markerRadius: Float,
     var yAxisMin: Float,
     var yAxisMax: Float,
     var color: String,
