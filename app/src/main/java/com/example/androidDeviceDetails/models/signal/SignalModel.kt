48976ed2f497d8e25064f4227dde29f73f861042
neena38 <neena.mec@gmail.com>
2021-02-10 17:01:18 +0530

    Refactoring.

--- app/src/main/java/com/example/androidDeviceDetails/models/signal/SignalModel.kt
+++ app/src/main/java/com/example/androidDeviceDetails/models/signal/SignalModel.kt
@@ -1,12 +1,12 @@
 package com.example.androidDeviceDetails.models.signal
 
 data class SignalCookedData(
-
     var roamingTime: Long = 0,
     var lastCellularStrength: Float = 0F,
     var mostUsedOperator: String = "Unknown",
     var mostUsedCellularBand: String = "Unknown",
 ) {
+
     var graphEntryList = arrayListOf<SignalGraphEntry>()
     var mostUsedWifiLevel: String = "Unknown"
     var lastWifiStrength: Float = 0F
@@ -14,7 +14,6 @@ data class SignalCookedData(
 }
 
 data class SignalGraphEntry(
-
     val timeStamp: Long,
     val signal: Int,
     val strength: Int,
